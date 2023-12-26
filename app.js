2048bf3d03bebdd60f4ca513b116fa82852c82c5
JOKER <88464595+Grizly9@users.noreply.github.com>
2023-12-26 22:26:47 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// app.js
+function handleUpload() {
+    // Handle file upload logic here
+    // For example, send the image to the Pexels API
+    // and display the enhanced image
+}
+
+document.querySelector('.upload-button').addEventListener('change', handleUpload);
